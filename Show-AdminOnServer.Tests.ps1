004370d1dc6705c556152a92ac6bca451152c938
PPadgett <Preston.Padgett@Outlook.com>
2021-04-05 12:44:05 -0500

    Expanded Pester Test Cases

--- Show-AdminOnServer.Tests.ps1
+++ Show-AdminOnServer.Tests.ps1
@@ -1,28 +1,38 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
-
-BeforeAll { 
-    function Show-AdminOnServer ([string]$TargetServer = '*') {
-        $ServerList = @(
-            @{ 'ServerName' = $env:COMPUTERNAME }
-            @{ 'FQDN' = ([System.Net.Dns]::GetHostByName(($env:COMPUTERNAME))).HostName}
-            @{ 'IP Address' = [ipaddress[]]([System.Net.Dns]::GetHostByName(($env:COMPUTERNAME))).AddressList}
-        ) | ForEach-Object { [PSCustomObject] $_ }    
-
-        $ServerList | Where-Object { $_.ServerName -or $_.FQDN -or $_.'IP Address' -like $TargetServer }
+Describe 'Show-AdminOnServer Valid Inputs' {
+    BeforeAll { 
+        function Show-AdminOnServer ([string]$TargetServer = '*') {
+            $DNSCheck = ([System.Net.Dns]::GetHostByName(("$env:COMPUTERNAME")))         
+            $Object = New-Object PSObject -Property ([ordered]@{ 
+              
+                        "Server name"             = $env:COMPUTERNAME
+                        "FQDN"                    = $DNSCheck.hostname
+                        "IP Address"              = $DNSCheck.AddressList[0]
+         
+            })
+            $Object | Where-Object { $_.Name -or $_.'FQDN' -or $_.'IP Address' -like $TargetServer }
+        }
     }
 }
 
-Describe 'Show-AdminOnServer' {
-    It 'Given no Target Server in parameters, it lists all 4 vaild connection Names' {
-        $allGroups = List-AdminOnServer
-        $allGroups | Should -be $true
+Describe 'Test for Vaild Connection Types' {
+    $AllTargets = Show-AdminOnServer
+    Function MockTest-Connection {Test-Connection -ComputerName $args[0] -Count 1 }
+    It "Test for Vaild Short Name Connection" {
+        $AllTargets.'Server Name' | Should Not BeNullOrEmpty
+        $AllTargets.'Server Name' | Should BeOfType [string]
+        MockTest-Connection $AllTargets.'Server Name'
+    }
+    It "Test for Vaild FQDN Name Connection" {
+        $AllTargets.'FQDN' | Should Not BeNullOrEmpty
+        $AllTargets.'FQDN' | Should BeOfType [string]
+        MockTest-Connection $AllTargets.'FQDN'
+    }
+    It "Test for Vaild IP Address Connection" {
+        $AllTargets.'IP Address' | Should Not BeNullOrEmpty
+        $AllTargets.'IP Address' | Should BeOfType [ipaddress]
+        MockTest-Connection $AllTargets.'IP Address'
     }
 }
-
-# Describe "List-AdminOnServer.ps1" {
-#     It "does something useful" {
-#         $true | Should Be $false
-#     }
-# }
